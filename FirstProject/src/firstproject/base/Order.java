2ff2d759a2a743c21b8612cbc8eb0d3c3655db7b
Allen Jordan <allenjordan65@gmail.com>
2022-03-25 11:13:55 -0300

    adicionada mais uma parte do javadoc na classe order

--- FirstProject/src/firstproject/base/Order.java
+++ FirstProject/src/firstproject/base/Order.java
@@ -22,6 +22,12 @@ public class Order {
         this.totalValue = totalValue;
     }
     
+    /**
+     * Calcula as taxas de acordo com o valor total do pedido. Se o pedido for maior que R$100,00 uma taxa é cobrada.
+     * 
+     * @return Valor total do pedido com as taxas.
+     * @throws RuntimeException Se o valor do pedido for negativo.
+     */
     public BigDecimal calculateFee() throws RuntimeException {
         if (this.totalValue.signum() < 0) {
             throw new RuntimeException("O pedido nao pode ter valor negativo");
